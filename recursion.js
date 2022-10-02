affacf70ddbf7cc9ca4af9343e83efde8e21a2c5
ryan <ryanjgecelter@gmail.com>
2022-10-02 11:50:30 +0300

    added functions getStrings and binarySearch

--- recursion.js
+++ recursion.js
@@ -46,27 +46,50 @@ function isPalindrome(str, i = str.length - 1, reverseStr = '') {
 
 /** findIndex: return the index of val in arr (or -1 if val is not present). */
 
-function findIndex(arr, val) {
-
+function findIndex(arr, val, i = 0) {
+  if (arr[i] === val) {
+    return i;
+  } else if (i >= arr.length - 1) {
+    return -1;
+  }
+  return findIndex(arr, val, i + 1);
 }
 
 /** revString: return a copy of a string, but in reverse. */
 
-function revString(str) {
-
+function revString(str, i = str.length - 1, reverseStr = '') {
+  if (i < 0) return reverseStr;
+  reverseStr += str[i];
+  return revString(str, i - 1, reverseStr);
 }
 
 /** gatherStrings: given an object, return an array of all of the string values. */
 
 function gatherStrings(obj) {
-
+  let stringArr = [];
+  for (key in obj) {
+    if (typeof obj[key] === 'string') stringArr.push(obj[key]);
+    if (typeof obj[key] === 'object') stringArr.push(...gatherStrings(obj[key]))
+  }
+  return stringArr;
 }
 
 /** binarySearch: given a sorted array of numbers, and a value,
  * return the index of that value (or -1 if val is not present). */
 
-function binarySearch(arr, val) {
-
+function binarySearch(arr, val, left = 0, right = arr.length - 1) {
+  if (left > right) return -1;
+  let middle = Math.floor((left + right) / 2);
+  console.log(middle);
+  if (arr[middle] === val) {
+    return middle;
+  }
+  else if (arr[middle] < val) {
+    return binarySearch(arr, val, left = middle + 1, right);
+  }
+  else {
+    return binarySearch(arr, val, left, right = middle - 1);
+  }
 }
 
 module.exports = {
